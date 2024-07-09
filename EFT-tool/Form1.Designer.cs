a691b66d44a2d19b7c7f72dfbd4d12edf3b71d84
fastwalkergy <106460370+fastwalkergy@users.noreply.github.com>
2024-07-09 23:34:54 +0800

    u 24709-1-1

---
+++ EFT-tool/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace EFT_tool
+{
+    partial class main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.addreg = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addreg
+            // 
+            this.addreg.Location = new System.Drawing.Point(57, 54);
+            this.addreg.Name = "addreg";
+            this.addreg.Size = new System.Drawing.Size(113, 56);
+            this.addreg.TabIndex = 0;
+            this.addreg.Text = "增加注册表";
+            this.addreg.UseVisualStyleBackColor = true;
+            this.addreg.Click += new System.EventHandler(this.addreg_Click);
+            // 
+            // main
+            // 
+            this.BackgroundImage = global::EFT_tool.Properties.Resources.tkf_l3r5vq_1280x720;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(613, 396);
+            this.Controls.Add(this.addreg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main";
+            this.Text = "EFT-tool";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addreg;
+    }
+}
+
---
+++ EFT-tool/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFT_tool
+{
+    public partial class main : Form
+    {
+
+        // 完整的注册表路径
+        string keyName = "SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\EscapeFromTarkov";
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void addreg_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 创建或打开注册表项
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(keyName))
+                {
+                    // 添加一个名为 "Version" 的字符串值
+                    key.SetValue("Version", "1.0");
+                    MessageBox.Show("注册表增加成功", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
